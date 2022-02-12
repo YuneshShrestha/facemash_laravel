e1aa91c0f3b95926bd24d411bcd2a0941a59c436
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2022-02-12 22:40:00 +0545

    models and migration created

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+  
+    public static function expected()
+    {
+        //
+    }
+
+
+    public static function win()
+    {
+        //
+    }
+
+    public static function loss()
+    {
+        //
+    }
+
+  
+    public function rank()
+    {
+        //
+    }
+}
